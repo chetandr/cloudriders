07de08114f1056ab7eac87d50146b76a40e5700e
Hemant Ghodke <hemant_ghodke@persistent.co.in>
2019-03-07 16:45:41 +0530

    Initial commit

---
+++ CloudriderAndroid/app/src/main/java/com/cloudrider/semicolon/CloudriderApp.java
@@ -0,0 +1,12 @@
+package com.cloudrider.semicolon;
+
+import android.app.Application;
+
+public class CloudriderApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
