ad28478b7cc0ef17cf8af7b5cda56601ba6d6e03
guruhosmath <gurulingayya_hosmath@persistent.co.in>
2019-03-09 11:32:39 +0530

    Apis for channel

--- cloudRiderAPI/app.js
+++ cloudRiderAPI/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const channelService = require('./services/channelService.js');
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -8,4 +9,20 @@ const port = 3000
 
 app.get('/', (req, res) => res.send({"message":"Hellooooooothere!!!"}))
 
+app.post('/channels',channelService.createChannel);
+
+app.get('/channels/:name',channelService.readChannel);
+
+app.put('/channels/:name',channelService.updateChannel);
+
+app.delete('/channels/:name',channelService.deleteChannel);
+
+app.get('/listChannels',channelService.listChannels);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/*
+list channels
+deploy invoke chaincode
+list ledger and list chaincode
+*/
