6157015ba5d371aeac5190dd6a1eed81d9addb01
Priyanka Deshmukh <priyankadeshmukh070@gmail.com>
2019-03-09 11:21:24 +0530

    API: consortium added.

--- cloudRiderAPI/app.js
+++ cloudRiderAPI/app.js
@@ -4,14 +4,21 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var logger = require('tracer').console();
-var org =require('./services/organization')
+var org = require('./services/organization')
+var consortium = require('./services/consortium')
 
 const port = 3000
 logger.info("Hello world")
 app.get('/', (req, res) => res.send({"message":"Hellooooooothere!!!"}))
-//organization
 
-app.get('/organization',org.getOrg)
-app.post('/organization', org.createOrg)
-app.delete('/organization',org.delOrg)
+//API for organization
+app.get('/hyperverse/organization',org.getOrg)
+app.get('/hyperverse/organization:name',org.getOrgByName)
+app.post('/hyperverse/organization', org.createOrg)
+app.delete('/hyperverse/organization:name',org.delOrgByName)
+
+//API for consortium
+app.get('/hyperverse/consortium:name',  consortium.getConsortiumByName)
+app.post('/hyperverse/consortium', consortium.createConsortium)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
