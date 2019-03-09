544029cee488dddd49df643a8272eb76a5d07c07
Ashwini Adsare <ashwini_adsare@persistent.co.in>
2019-03-10 00:19:54 +0530

    corrected grammer

--- cloudRiderAdminUI/src/app/components/sidebar/sidebar.component.ts
+++ cloudRiderAdminUI/src/app/components/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: 'material-icons' },
-    { path: '/consortium', title: 'Consortium',  icon: '', class: 'fa fa-connectdevelop' },
+    { path: '/consortium', title: 'Consortiums',  icon: '', class: 'fa fa-connectdevelop' },
     { path: '/orgs', title: 'Organisations',  icon:'', class: 'fa fa-first-order' },
     { path: '/peers', title: 'Peers',  icon:'device_hub', class: 'material-icons' },
     { path: '/channels', title: 'Channels',  icon:'', class: 'fa fa-podcast' }
