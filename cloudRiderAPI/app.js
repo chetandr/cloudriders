f3f8342d14d450856d6754b1053239158f2be2c2
guruhosmath <gurulingayya_hosmath@persistent.co.in>
2019-03-10 01:22:48 +0530

    CORS support

--- cloudRiderAPI/app.js
+++ cloudRiderAPI/app.js
@@ -4,6 +4,11 @@ const channelService = require('./services/channelService.js');
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var logger = require('tracer').console();
 var org =require('./services/organization')
 var peers =require('./services/peers')
