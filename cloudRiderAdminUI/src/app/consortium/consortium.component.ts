0e482fd737baffeba2c85c3a6e1d953f29809746
Ashwini Adsare <ashwini_adsare@persistent.co.in>
2019-03-10 07:41:33 +0530

    added UI

--- cloudRiderAdminUI/src/app/consortium/consortium.component.ts
+++ cloudRiderAdminUI/src/app/consortium/consortium.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'consortium',
@@ -9,16 +9,41 @@ import { HttpClient } from '@angular/common/http';
 export class ConsortiumComponent implements OnInit {
 
   consordiumData = {};
+  obj : any = {};
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
   constructor(private http: HttpClient) { 
     this.http = http;
   }
 
   ngOnInit() {
-    this.http.get("http://10.44.14.143:3000/hyperverse/consortium").subscribe((data: any) => {
+    this.http.get("http://10.44.14.136:3000/hyperverse/consortium").subscribe((data: any) => {
           console.log(data.data); 
           this.consordiumData=data.data;
                             
         });
   }
+  
+   onClick(){
+    
+    console.log(this.obj);
+    let data = this.http.post(
+      "http://10.44.14.136:3000/hyperverse/consortium",
+     JSON.stringify(this.obj),
+      {headers: this.headers}
+    ).subscribe((response:any)=>{
+      console.log(response);
+      
+    },(error:any)=>{
+      console.log(error);
+      
+    })
+    return ''
+
+  }
 
 }
+
+
+
